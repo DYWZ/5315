e94982f357e8674840cc124e66b19b4c82a0b8b9
irene <yangningyun@seattleu.edu>
2019-04-28 12:20:20 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -9,44 +9,12 @@
       "language": "Korean"
     },
     {
-      "title": "White Noise",
-      "singer": "EXO",
-      "length": "3:32",
-      "album": "Ex'Act",
-      "year": "2016",
+      "title": "Gashina",
+      "singer": "Sunmi",
+      "length": "3:00",
+      "album": "Warning",
+      "year": "2018",
       "language": "Korean"
-    },
-    {
-      "title": "James Bond",
-      "singer": "Accountant",
-      "length": "$170,750",
-      "album": "2011/07/25",
-      "year": "Tokyo",
-      "language": "8422"
-    },
-    {
-      "title": "James Bond",
-      "singer": "Accountant",
-      "length": "$170,750",
-      "album": "2011/07/25",
-      "year": "Tokyo",
-      "language": "8422"
-    },
-    {
-      "title": "James Bond",
-      "singer": "Accountant",
-      "length": "$170,750",
-      "album": "2011/07/25",
-      "year": "Tokyo",
-      "language": "8422"
-    },
-    {
-      "title": "James Bond",
-      "singer": "Accountant",
-      "length": "$170,750",
-      "album": "2011/07/25",
-      "year": "Tokyo",
-      "language": "8422"
     },
     {
       "title": "Lotto",
@@ -55,6 +23,38 @@
       "album": "Ex'Act",
       "year": "2016",
       "language": "Korean"
+    },
+    {
+      "title": "White Noise",
+      "singer": "EXO",
+      "length": "3:32",
+      "album": "Ex'Act",
+      "year": "2016",
+      "language": "Korean"
+    },
+    {
+      "title": "What Do You Mean",
+      "singer": "Justin Bieber",
+      "length": "3:25",
+      "album": "Purpose",
+      "year": "2015",
+      "language": "English"
+    },
+    {
+      "title": "Sorry",
+      "singer": "Justin Bieber",
+      "length": "3:20",
+      "album": "Purpose",
+      "year": "2015",
+      "language": "English"
+    },
+    {
+      "title": "Love Yourself",
+      "singer": "Justin Bieber",
+      "length": "3:53",
+      "album": "Purpose",
+      "year": "2015",
+      "language": "English"
     },
     {
       "title": "James Bond",
