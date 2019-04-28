26ee019b729ff3ea7a73a44f3d212b11c86d29ee
irene <yangningyun@seattleu.edu>
2019-04-28 12:10:00 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -97,12 +97,12 @@
       "language": "8422"
     },
     {
-      "title": "James Bond",
-      "singer": "Accountant",
-      "length": "$170,750",
-      "album": "2011/07/25",
-      "year": "Tokyo",
-      "language": "8422"
+      "title": "Gashina",
+      "singer": "Sunmi",
+      "length": "3:00",
+      "album": "Warning",
+      "year": "2018",
+      "language": "Korean"
     },
     {
       "title": "James Bond",
